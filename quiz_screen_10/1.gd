fb4e27370c756ac01bf0072ce425ccb1ceb14fd7
tylerbert31 <101559620+tylerbert31@users.noreply.github.com>
2022-10-31 22:56:56 +0800

    question counter
    question counter

---
+++ quiz_screen_10/1.gd
@@ -0,0 +1,30 @@
+extends Label
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+var number = 1
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_next_pressed():
+	
+	if number < 10:
+		number += 1
+		self.text = str(number)
+
+
+func _on_prev_pressed():
+	if number > 1:
+		number -= 1
+		self.text = str(number)
